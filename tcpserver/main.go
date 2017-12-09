862edda6bf02c5a2db2718869bdc49996389b2f0
Kyaw Thant Tin <kyawthanttin@gmail.com>
2017-12-10 00:43:04 +0630

    ThreadedEchoServer

---
+++ tcpserver/main.go
@@ -0,0 +1,53 @@
+/* ThreadedEchoServer
+ */
+package tcpserver
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	listener, err := net.Listen("tcp", ":1201")
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		// run as a goroutine
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	// close connection on exit
+	defer conn.Close()
+
+	var buf [512]byte
+	for {
+		// read upto 512 bytes
+		n, err := conn.Read(buf[0:])
+		if err != nil {
+			return
+		}
+
+		fmt.Println("Message received: ", n)
+
+		// write the n bytes read
+		_, err2 := conn.Write(buf[0:n])
+		if err2 != nil {
+			return
+		}
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
