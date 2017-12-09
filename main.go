f0341eb80bad4c5709cc957be9ba009baa916f41
Kyaw Thant Tin <kyawthanttin@gmail.com>
2017-12-09 18:17:32 +0630

    Test

--- main.go
+++ main.go
@@ -1,55 +1,29 @@
-/* ThreadedEchoServer
- */
 package main
 
-import (
-	"fmt"
-	"net"
-	"os"
-)
+import "net"
+import "fmt"
+import "bufio"
+import "strings" // only needed below for sample processing
 
 func main() {
 
-	service := ":1201"
-	tcpAddr, err := net.ResolveTCPAddr("ip4", service)
-	checkError(err)
+	fmt.Println("Launching server...")
 
-	listener, err := net.ListenTCP("tcp", tcpAddr)
-	checkError(err)
+	// listen on all interfaces
+	ln, _ := net.Listen("tcp", ":8081")
 
+	// accept connection on port
+	conn, _ := ln.Accept()
+
+	// run loop forever (or until ctrl-c)
 	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			continue
-		}
-		// run as a goroutine
-		go handleClient(conn)
-	}
-}
-
-func handleClient(conn net.Conn) {
-	// close connection on exit
-	defer conn.Close()
-
-	var buf [512]byte
-	for {
-		// read upto 512 bytes
-		n, err := conn.Read(buf[0:])
-		if err != nil {
-			return
-		}
-
-		// write the n bytes read
-		_, err2 := conn.Write(buf[0:n])
-		if err2 != nil {
-			return
-		}
-	}
-}
-
-func checkError(err error) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		os.Exit(1)
+		// will listen for message to process ending in newline (\n)
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		// output message received
+		fmt.Print("Message Received:", string(message))
+		// sample process for string received
+		newmessage := strings.ToUpper(message)
+		// send new string back to client
+		conn.Write([]byte(newmessage + "\n"))
 	}
 }
